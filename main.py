a7e129c9c3a49c113afcbc08ddd5ea83476ff579
Ramiro Ramirez-Sanchez <45070898+ramirors1@users.noreply.github.com>
2022-10-07 14:26:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()   
+
+    #print (f"Hello {formatted_f_name} {formatted_l_name}.")
+    return f"{formatted_f_name} {formatted_l_name}"
+
+#format_name("angela", "booyeah")    
+print(format_name("angela", "booyeah"))
+
